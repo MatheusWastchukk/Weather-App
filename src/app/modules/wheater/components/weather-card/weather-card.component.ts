7f34ffaf5ba722b36e30c417666c32e28ee44ba7
MatheusWastchukk <matheuswastchuk@gmail.com>
2024-07-10 17:10:38 -0300

    criação e implementação do componente weather-card

--- src/app/modules/wheater/components/weather-card/weather-card.component.ts
+++ src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
+import { Component, Input} from '@angular/core';
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: []
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent{
+  @Input() weatherDatasInput!: WeatherDatas;
+    //Dados recebidos pelo componente pai
 
-}
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+  }
